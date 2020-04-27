574b3f0494dac02b4ff614aee033cc768bac3a7f
Sanyam Jain <56352967+demonnezuko@users.noreply.github.com>
2020-04-27 12:40:56 +0530

    Create test_script.sh

---
+++ build_tools/travis/test_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This script is meant to be called by the "script" step defined in
+# .travis.yml. See https://docs.travis-ci.com/ for more details.
+# The behavior of the script is controlled by environment variabled defined
+# in the .travis.yml in the top level folder of the project.
+
+# License: 3-clause BSD
+
+set -e
+
+python --version
+
+run_tests() {
+    TEST_CMD="python test.py -vv"
+    
+    if [[ "$COVERAGE" == "true" ]]; then
+      TEST_CMD="$TEST_CMD --coverage"
+    fi
+}
+
+run_tests
