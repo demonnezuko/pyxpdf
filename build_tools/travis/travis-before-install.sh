ad4de88bf6231d3ff8be4a925cfe0f92e6f31bc3
Sanyam Jain <56352967+demonnezuko@users.noreply.github.com>
2020-04-27 21:23:18 +0530

    Create travis-before-install.sh

---
+++ build_tools/travis/travis-before-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Exit the script immediately if a command exits with a non-zero status,
+# and print commands and their arguments as they are executed.
+set -ex
+
+uname -a
+free -m
+df -h
+ulimit -a
+
+mkdir builds
+pushd builds
+
+python -V
+gcc --version
+
+popd
+
+pip install --upgrade pip
+
+pip install setuptools wheel cython 
+
+if [ -n "$RUN_COVERAGE" ]; then pip install coverage; fi
