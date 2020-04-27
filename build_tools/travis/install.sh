709726863a0e9724b498a2b22bf97a65bb64caf7
Sanyam Jain <56352967+demonnezuko@users.noreply.github.com>
2020-04-27 13:08:16 +0530

    [skip ci] more fixes

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
-# adapted from https://github.com/scikit-learn/scikit-learn/tree/master/build_tools/travis/install.sh
-# This script is meant to be called by the "install" step defined in
-# .travis.yml. See https://docs.travis-ci.com/ for more details.
-# The behavior of the script is controlled by environment variabled defined
-# in the .travis.yml in the top level folder of the project.
-
-# License: 3-clause BSD
-
 set -e
-
-echo "List files from cached directories"
-echo "pip:"
-ls $HOME/.cache/pip
-
-export CC=/usr/lib/ccache/gcc
-export CXX=/usr/lib/ccache/g++
-# Useful for debugging how ccache is used
-# export CCACHE_LOGFILE=/tmp/ccache.log
-# ~60M is used by .ccache when compiling from scratch at the time of writing
-ccache --max-size 100M --show-stats
 
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
@@ -27,9 +8,8 @@ SETUP_OPTS="--with-coverage"
 if [ "$CHECK_WARNINGS" == "true" ]; then
   SETUP_OPTS="$SETUP_OPTS --warnings"
 fi
+if [ "$WITH_CYTHON" == "true" ]; then
+  SETUP_OPTS="$SETUP_OPTS --with-cython"
+fi
 python setup.py develop $SETUP_OPTS
-
-ccache --show-stats
-# Useful for debugging how ccache is used
-# cat $CCACHE_LOGFILE
 
